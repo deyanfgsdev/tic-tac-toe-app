d76b3a77ad9f37e5a9e63d2352c428fed15b51c7
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-03-11 12:55:39 +0000

    feat: create rrss data

---
+++ src/data/rrssData.js
@@ -0,0 +1,25 @@
+import {
+  faGithub,
+  faLinkedinIn,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+
+const rrssData = [
+  {
+    id: 1,
+    icon: faGithub,
+    url: "https://github.com/deyanfgsdev",
+  },
+  {
+    id: 2,
+    icon: faLinkedinIn,
+    url: "https://www.linkedin.com/in/deyanfgsdev/",
+  },
+  {
+    id: 3,
+    icon: faXTwitter,
+    url: "https://twitter.com/deyanfgs",
+  },
+];
+
+export default rrssData;
