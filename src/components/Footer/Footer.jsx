faa1513ad950122f7b65ba7ce1a7063a9777b856
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-03-11 15:39:53 +0000

    feat: create footer layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,20 @@
 import "./Footer.scss";
+import rrssData from "../../data/rrssData";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = () => {
+  const rrssList = rrssData.map((item) => (
+    <a href={item.url} target="_blank" key={item.id}>
+      <FontAwesomeIcon icon={item.icon} />
+    </a>
+  ));
+
   return (
     <footer>
       <address className="tic-tac-toe-footer-author">
-        Author: Deyán Fabricio Guacarán Sabogal
+        &copy; 2024 Developed by deyanfgsdev
       </address>
+      <div className="tic-tac-toe-footer-rrss">{rrssList}</div>
     </footer>
   );
 };
