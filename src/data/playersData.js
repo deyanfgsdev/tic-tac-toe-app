ac0cfdc399ab88fcc8a70e499dc876fc285565cf
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-03-13 13:05:37 +0000

    feat: create initial players data

---
+++ src/data/playersData.js
@@ -0,0 +1,20 @@
+const playersData = [
+  {
+    id: 1,
+    name: null,
+    isYourTurn: false,
+    winner: false,
+    imgSrc: "https://i.ibb.co/hY3BRcC/tic-tac-toe-player-1.png",
+    alt: "Player 1",
+  },
+  {
+    id: 2,
+    name: null,
+    isYourTurn: false,
+    winner: false,
+    imgSrc: "https://i.ibb.co/3h3CVQV/tic-tac-toe-player-2.png",
+    alt: "Player 2",
+  },
+];
+
+export default playersData;
