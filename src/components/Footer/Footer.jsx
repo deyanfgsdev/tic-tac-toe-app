ecb3ef0346369cc43ffd16de150c664682dc3475
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-09-04 12:15:54 +0100

    refactor: about Footer Component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,18 +3,18 @@ import rrssData from "../../data/rrssData";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = () => {
-  const rrssList = rrssData.map((item) => (
-    <a href={item.url} target="_blank" key={item.id}>
-      <FontAwesomeIcon icon={item.icon} />
-    </a>
-  ));
-
   return (
     <footer>
-      <address className="tic-tac-toe-footer-author">
+      <address className="tic-tac-toe--footer-author">
         &copy; 2024 Developed by deyanfgsdev
       </address>
-      <div className="tic-tac-toe-footer-rrss">{rrssList}</div>
+      <div className="tic-tac-toe--footer-rrss">
+        {rrssData.map((item) => (
+          <a href={item.url} target="_blank" key={item.id}>
+            <FontAwesomeIcon icon={item.icon} />
+          </a>
+        ))}
+      </div>
     </footer>
   );
 };
