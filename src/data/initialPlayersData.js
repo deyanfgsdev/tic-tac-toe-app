6abdb4b232f454c955c3006e2af32733fb691ba6
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-09-04 15:01:32 +0100

    refactor: rename players Data file

--- src/data/initialPlayersData.js
+++ src/data/initialPlayersData.js
@@ -1,20 +1,16 @@
-const playersData = [
+const initialPlayersData = [
   {
     id: 1,
     name: "Player 1",
-    isYourTurn: false,
-    winner: false,
     imgSrc: "https://i.ibb.co/hY3BRcC/tic-tac-toe-player-1.png",
     alt: "Player 1",
   },
   {
     id: 2,
     name: "Player 2",
-    isYourTurn: false,
-    winner: false,
     imgSrc: "https://i.ibb.co/3h3CVQV/tic-tac-toe-player-2.png",
     alt: "Player 2",
   },
 ];
 
-export default playersData;
+export default initialPlayersData;
