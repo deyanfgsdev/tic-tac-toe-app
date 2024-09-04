1e12bcd792927abc304ea218178c0d1ae52a78d6
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-09-04 11:46:21 +0100

    refactor: improve App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,18 @@ import "./App.scss";
 import Logo from "./components/Logo/Logo";
 import Board from "./components/Board/Board";
 import Footer from "./components/Footer/Footer";
-import Player from "./components/Player/Player";
-
-import playersData from "./data/playersData";
 
 const App = () => {
   const logoSrc = "https://i.ibb.co/CHJkpRX/tic-tac-toe-logo.png";
-  const playersList = playersData.map((player) => {
-    return (
-      <li className="tic-tac-toe-player" key={player.id}>
-        <Player
-          playerId={player.id}
-          initialPlayerName={player.name}
-          initialCurrentTurn={player.isYourTurn}
-          initialIsWinner={player.winner}
-          imgSrc={player.imgSrc}
-          alt={player.alt}
-        />
-      </li>
-    );
-  });
 
   return (
-    <>
-      <div className="tic-tact-toe-app">
-        <div className="tic-tac-tope-content-wrap">
-          <Logo logoSrc={logoSrc} />
-          <Board />
-          <div className="tic-tac-toe-players">
-            <ul className="tic-tac-toe-players-list">{playersList}</ul>
-          </div>
-        </div>
-        <Footer />
+    <div className="tic-tact-toe--app">
+      <div className="tic-tac-toe--content-wrap">
+        <Logo logoSrc={logoSrc} />
+        <Board />
       </div>
-    </>
+      <Footer />
+    </div>
   );
 };
 
