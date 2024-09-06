51c69553db80564d6bf4bdcf58ceb8c1adfe421e
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-09-06 11:07:19 +0100

    refactor: initial players data

--- src/data/initialPlayersData.js
+++ src/data/initialPlayersData.js
@@ -1,14 +1,16 @@
+const MAIN_IMAGES_URL = "https://i.ibb.co";
+
 const initialPlayersData = [
   {
     id: 1,
     name: "Player 1",
-    imgSrc: "https://i.ibb.co/hY3BRcC/tic-tac-toe-player-1.png",
+    imgSrc: `${MAIN_IMAGES_URL}/hY3BRcC/tic-tac-toe-player-1.png`,
     alt: "Player 1",
   },
   {
     id: 2,
     name: "Player 2",
-    imgSrc: "https://i.ibb.co/3h3CVQV/tic-tac-toe-player-2.png",
+    imgSrc: `${MAIN_IMAGES_URL}/3h3CVQV/tic-tac-toe-player-2.png`,
     alt: "Player 2",
   },
 ];
