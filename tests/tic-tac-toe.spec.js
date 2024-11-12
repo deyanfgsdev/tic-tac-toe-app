b071eef19235255cf656741f1a0692b34cf1fe44
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-11-12 17:08:55 +0000

    wip: test about should have 9 empty cells

--- tests/tic-tac-toe.spec.js
+++ tests/tic-tac-toe.spec.js
@@ -12,4 +12,22 @@ test.describe('Board', () => {
     const cells = await page.locator('.tic-tac-toe--board > .tic-tac-toe--square')
     await expect(cells).toHaveCount(9)
   })
+
+  test('should have 9 empty cells', async ({ page }) => {
+    const cells = await page.locator('.tic-tac-toe--board > .tic-tac-toe--square')
+    const cellsCount = await cells.count()
+
+    const firstCell = await cells.first()
+    await firstCell.click()
+    const cellImage = await firstCell.locator('img')
+    await expect(cellImage).toBeVisible()
+
+    for (let i = 0; i < cellsCount; i++) {
+      const cell = await cells.nth(i)
+      await expect(cell).toBeEmpty()
+
+      // const cellPlayerImage = await cell.locator('img')
+      // await expect(cellPlayerImage).not.toBeVisible()
+    }
+  })
 })
