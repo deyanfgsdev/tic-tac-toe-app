58ac2e43a62276849483c3f51cea41025af56655
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-12-05 11:18:10 +0000

    refactor: about main format

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-import "./index.scss";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/700.css";
+import './index.scss';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/700.css';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
