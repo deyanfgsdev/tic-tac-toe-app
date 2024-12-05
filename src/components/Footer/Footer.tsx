038a4bbe507e7cbe83d98e789e820541a3554f10
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-12-05 12:35:44 +0000

    refactor: convert Footer component from JS to TS

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,22 @@
+import './Footer.scss';
+import rrssData from '../../data/rrssData';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Footer = () => {
+  return (
+    <footer>
+      <address className="tic-tac-toe--footer-author">
+        &copy; 2024 Developed by deyanfgsdev
+      </address>
+      <div className="tic-tac-toe--footer-rrss">
+        {rrssData.map((item) => (
+          <a href={item.url} target="_blank" key={item.id} rel="noreferrer">
+            <FontAwesomeIcon icon={item.icon} />
+          </a>
+        ))}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/data/rrssData.ts
@@ -0,0 +1,27 @@
+import {
+  faGithub,
+  faLinkedinIn,
+  faXTwitter,
+} from '@fortawesome/free-brands-svg-icons';
+
+import { SocialMediaList } from '../types/global';
+
+const rrssData: SocialMediaList = [
+  {
+    id: 1,
+    icon: faGithub,
+    url: 'https://github.com/deyanfgsdev',
+  },
+  {
+    id: 2,
+    icon: faLinkedinIn,
+    url: 'https://www.linkedin.com/in/deyanfgsdev/',
+  },
+  {
+    id: 3,
+    icon: faXTwitter,
+    url: 'https://twitter.com/deyanfgs',
+  },
+];
+
+export default rrssData;
