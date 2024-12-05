16523b37dc00584169befab35ec0026e805c0eed
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-12-05 13:03:31 +0000

    refactor: convert initialPlayersData from JS to TS

---
+++ src/data/initialPlayersData.ts
@@ -0,0 +1,20 @@
+import { PlayerList } from '../types/global';
+
+const MAIN_IMAGES_URL = 'https://i.ibb.co';
+
+const initialPlayersData: PlayerList = [
+  {
+    id: 1,
+    name: 'Player 1',
+    imgSrc: `${MAIN_IMAGES_URL}/hY3BRcC/tic-tac-toe-player-1.png`,
+    alt: 'Player 1',
+  },
+  {
+    id: 2,
+    name: 'Player 2',
+    imgSrc: `${MAIN_IMAGES_URL}/3h3CVQV/tic-tac-toe-player-2.png`,
+    alt: 'Player 2',
+  },
+];
+
+export default initialPlayersData;
