eb5bc7aca5249d17d05a48740cb0629654ae6082
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-12-06 17:56:53 +0000

    refactor: convert Square component from JS to TS

---
+++ src/components/Square/Square.tsx
@@ -0,0 +1,36 @@
+import './Square.scss';
+
+import initialPlayersData from '../../data/initialPlayersData';
+
+const Square = ({
+  children,
+  index,
+  updateBoard,
+}: {
+  children: string | null;
+  index: number;
+  updateBoard: (index: number) => void;
+}) => {
+  const playerAlt =
+    children === initialPlayersData[0].imgSrc
+      ? initialPlayersData[0].alt
+      : initialPlayersData[1].alt;
+
+  const handleSquareClick = () => {
+    updateBoard(index);
+  };
+
+  return (
+    <div className="tic-tac-toe--square" onClick={handleSquareClick}>
+      {children && (
+        <img
+          src={children}
+          alt={playerAlt}
+          className="tic-tac-toe--square-img"
+        />
+      )}
+    </div>
+  );
+};
+
+export default Square;
