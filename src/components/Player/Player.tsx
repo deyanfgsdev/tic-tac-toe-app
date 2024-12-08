9c354965681a4c2eae3a8569b34876af7c8e3bc4
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-12-08 12:04:13 +0000

    refactor: convert Player component from JS to TS

---
+++ src/components/Player/Player.tsx
@@ -0,0 +1,47 @@
+import './Player.scss';
+
+import { Player as GlobalPlayer } from '../../types/global';
+
+const Player = ({
+  name,
+  imgSrc,
+  alt,
+  isYourTurn,
+  checkShowEditModal,
+}: {
+  name: GlobalPlayer['name'];
+  imgSrc: GlobalPlayer['imgSrc'];
+  alt: string;
+  isYourTurn: boolean;
+  checkShowEditModal: (
+    newImgSrc: GlobalPlayer['imgSrc'],
+    showModal: boolean
+  ) => void;
+}) => {
+  const handleChangeNameClick = (imgSrc: GlobalPlayer['imgSrc']) => {
+    checkShowEditModal(imgSrc, true);
+  };
+
+  const className = isYourTurn
+    ? 'tic-tac-toe--img-container is-your-turn'
+    : 'tic-tac-toe--img-container';
+
+  return (
+    <>
+      <div className="tic-tac-toe--player">
+        <div className={className}>
+          <img src={imgSrc} alt={alt} className="tic-tac-toe-player--img" />
+        </div>
+        <h2 className="tic-tac-toe-player--name">{name}</h2>
+        <button
+          className="tic-tac-toe-player--change-name-button"
+          onClick={() => handleChangeNameClick(imgSrc)}
+        >
+          Change name
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Player;
