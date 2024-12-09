71a4ef1f882242b0d1bdbbc9c04443b61591eb55
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-12-09 11:02:40 +0000

    refactor: convert tic tac toe test from JS to TS

---
+++ tests/tic-tac-toe.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+
+const LOCALHOST_URL = 'http://localhost:5173/';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(LOCALHOST_URL);
+});
+
+test.describe('Board', () => {
+  test('should have 9 cells', async ({ page }) => {
+    const cells = page.locator('.tic-tac-toe--board > .tic-tac-toe--square');
+    await expect(cells).toHaveCount(9);
+  });
+
+  test('should have 9 empty cells', async ({ page }) => {
+    const cells = page.locator('.tic-tac-toe--board > .tic-tac-toe--square');
+    const cellsCount = await cells.count();
+
+    const firstCell = cells.first();
+    await firstCell.click();
+    const cellImage = firstCell.locator('img');
+    await expect(cellImage).toBeVisible();
+
+    for (let i = 0; i < cellsCount; i++) {
+      const cell = cells.nth(i);
+      await expect(cell).toBeEmpty();
+
+      // const cellPlayerImage = await cell.locator('img')
+      // await expect(cellPlayerImage).not.toBeVisible()
+    }
+  });
+});
